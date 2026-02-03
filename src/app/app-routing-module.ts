bd5e1463561939d68ecb3a3f166fa3d77cb1e70d
SABELO  P. ZULU <ZULUPR@KZNPREMIER.GOV.ZA>
2026-02-03 12:44:36 +0200

    Removing silindelo

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Signup} from './features/signup/signup';
+import {Signin} from './features/signin/signin';
+import {Home} from './features/home/home';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'signup', component: Signup },
+  { path: 'signin', component: Signin },
+  { path: 'home', component: Home },
+  { path: '', redirectTo: '/signin', pathMatch: 'full' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
