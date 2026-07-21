582f66ec8340b6317c151b9fe068d72d5f909b1f
SABELO  P. ZULU <ZULUPR@KZNPREMIER.GOV.ZA>
2026-07-21 14:52:13 +0200

    bugfixing

--- src/app/services/state-car-log-service.ts
+++ src/app/services/state-car-log-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface StateCarLog {
   id?: number;
@@ -16,31 +17,32 @@ export interface StateCarLog {
   providedIn: 'root',
 })
 export class StateCarLogService {
-private apiUrl = 'http://localhost:8080/api/state-cars';
+
+  private apiUrl = `${environment.apiUrl}/state-cars`;
 
   constructor(private http: HttpClient) {}
 
-  /** Create a new log */
   createLog(log: StateCarLog): Observable<StateCarLog> {
     return this.http.post<StateCarLog>(`${this.apiUrl}/log`, log);
   }
 
-  /** Get all logs */
   getAllLogs(): Observable<StateCarLog[]> {
     return this.http.get<StateCarLog[]>(`${this.apiUrl}/logs`);
   }
 
-  /** Get a single log by ID */
   getLog(id: number): Observable<StateCarLog> {
     return this.http.get<StateCarLog>(`${this.apiUrl}/log/${id}`);
   }
-/** Delete a log by ID */
-deleteLog(id: number): Observable<any> {
-  return this.http.delete(`${this.apiUrl}/${id}`);
-}
-getVehicleImages(userId: string, registration: string) {
-  return this.http.get<string[]>(`http://localhost:8080/api/images/${userId}/${registration}`);
-}
+
+  deleteLog(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  getVehicleImages(userId: string, registration: string) {
+    return this.http.get<string[]>(
+      `${environment.apiUrl}/images/${userId}/${registration}`
+    );
+  }
 
 
 }
